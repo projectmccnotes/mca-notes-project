6e6543b3f5b4ae1a004fe5f7c46ea0daffbdd4db
shubhu-gitlogger <paygudeshubhankar8120@gmail.com>
2025-03-28 23:47:28 +0530

    final project

---
+++ include/header.php
@@ -0,0 +1,37 @@
+<header id="header">
+	<div id="head" class="parallax" parallax-speed="2">
+		<h1 id="logo" class="text-center">
+			<!-- <img class="img-circle" src="assets/images/guy.png" alt=""> -->
+			<span class="title">Dr. Rama Bansode</span>
+			<span class="tagline">Professor<br>
+				<a href="https://maps.app.goo.gl/dvSXvKU6ikefiGsw5" target="_blank">PES Modern College Of Engineering, PUNE</a></span>
+		</h1>
+	</div>
+
+	<nav class="navbar navbar-default navbar-sticky">
+		<div class="container-fluid">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			
+			<div class="navbar-collapse collapse">
+				
+				<ul class="nav navbar-nav">
+					<li><a href="index.html">Home</a></li>
+					<li class="active"><a href="about.html">About</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Notes<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="software-testing.html">Software Testing </a></li>
+							<li><a href="networking.html">Computer Networking</a></li>
+							<li><a href="discre-mathematics.html">Discrete Mathematics</a></li>
+						</ul>
+					</li>
+					<!-- <li><a href="blog.php">Blog</a></li> -->
+				</ul>
+			
+			</div><!--/.nav-collapse -->			
+		</div>	
+	</nav>
+</header>
